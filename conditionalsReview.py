17f36de2176af119331fb4dde2833fd297f1ae16
Morales-Galarza <eamorales-ga@cps.k12.il.us>
2024-05-07 09:15:45 -0500

    loops2

--- conditionalsReview.py
+++ conditionalsReview.py
@@ -14,15 +14,3 @@ elif weather == "cold":
 # If the input doesn't match any category, inform the user with a message saying the input was not recognized.
 else:
     print("Input not recognized")
-
-#Create a Python script that assigns a grade to a student based on their exam score.
-Ask the user to input a score. Assume the score is out of 100.
-Implement the logic to determine the grade based on the score:
-A score of 90 and above is an "A".
-A score of 80 to 89 is a "B".
-A score of 70 to 79 is a "C".
-A score of 60 to 69 is a "D".
-A score below 60 is an "F".
-Ensure the score entered is within the valid range (0-100). If not, print an error message.
-
-
